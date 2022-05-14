294e309b64a0fa291cb1b90868687dff69d4e326
Nischal Pandey <admin@nischalpandey.com.np>
2022-05-14 16:12:30 +0545

    Create pyauutogui.py

---
+++ Python/module/pyauutogui.py
@@ -0,0 +1,16 @@
+import pyautogui
+import time
+time.sleep(4)
+
+pyautogui.typewrite("Hero Bro ")
+for n in range(1, 31):
+   print(
+        pyautogui.typewrite("LOve U :-)")#str((n)))
+        
+       ,
+   pyautogui.press("enter")
+   ,
+   time.sleep(1)
+
+  )
+#visit https://github.com/asweigart/pyautogui
