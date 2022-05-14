e72cb15f7e7df3e026b1f860e0ec17fe4b32ef6a
Nischal Pandey <admin@nischalpandey.com.np>
2022-05-14 16:08:45 +0545

    Create Pywhaitkit.py

---
+++ Python/Pywhaitkit.py
@@ -0,0 +1,19 @@
+import pywhatkit 
+#photo to txt
+from pywhatkit.remotekit import start_server
+ #Here we are importing pywhatkit as nlm
+pywhatkit.image_to_ascii_art('nis.jpg','eee.txt') 
+
+# to handwriting.....
+
+
+
+#Version Information
+# ASCII Art
+# Exception
+# Miscellaneous Functions
+# Sending a Mail
+# Sending WhatsApp Messages
+# Shutdown and CancelShutdown
+# Text to Handwriting
+#visit https://github.com/Ankit404butfound/PyWhatKit/wiki
